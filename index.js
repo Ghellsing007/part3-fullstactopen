b42ca7ba2f0b5b69af1d00ae2ee025c8ca279ba6
Ghellsing007 <99853445+Ghellsing007@users.noreply.github.com>
2025-03-18 18:24:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
 
-console.log(process.env.SU)
-console.log(process.env.SALUDOS)
-console.log(process.env.MONGODB_URI)
 
 // Importa los middlewares personalizados
 const { unknownEndpoint, errorHandler } = require('./middleware/middleware.js')
